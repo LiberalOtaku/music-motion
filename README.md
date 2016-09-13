edb31eb6a818c8bcf9d0cdef5549732951e98aca
LiberalOtaku <ledgam3r1279@gmail.com>
2016-09-13 16:34:14 -0400

    Initial commit: draw dot at mouse position, added readme.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Music Motion
+
+A PC game developed as an Earlham College senior research project in collaboration with the Music and Computer Science Departments. More info to come soon.
+
+## Technical Overview
+
+* [OpenGL](https://www.opengl.org/)
+
+## Setup
+
+As of now, the program can simply be compiled with the following command.
+
+```shell
+$ gcc -Wall -ggdb music-motion.cpp -o music-motion -lGL -lglut -lGLU
+```
+
+Make sure you are in the same directory as the `music-motion.cpp` source file.
---
+++ music-motion.cpp
@@ -0,0 +1,66 @@
+// File: music-motion.cpp
+// Author: Edward Ly
+// Last Updated: 13 September 2016
+//
+// A PC rhythm game developed with OpenGL where you move a controller (e.g. mouse) in time with the music.
+// The game may support additional peripherals
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <GL/glui.h>
+
+struct GLintPoint {
+  int x, y;
+};
+
+const int windowWidth = 1280, windowHeight = 720;
+
+void myInit(void) {
+  glClearColor(0.8, 0.8, 0.8, 0.0); // set light gray background color
+  glColor3f(0.2f, 0.2f, 0.8f); // set the drawing color
+  glPointSize(4.0); // a ‘dot’ is 4 by 4 pixels
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0.0, static_cast<double>(windowWidth), 0.0, static_cast<double>(windowHeight));
+}
+
+void myDisplay(void) {
+  glClear(GL_COLOR_BUFFER_BIT); // clear the screen
+
+  glFlush(); // send all output to display
+}
+
+void myMovedMouse(int x, int y) {
+  static GLintPoint currentPosition;
+  currentPosition.x = x;
+  currentPosition.y = windowHeight - y;
+
+  glClear(GL_COLOR_BUFFER_BIT); // clear the screen
+
+  // do something at current mouse position
+  glBegin(GL_POINTS);
+  glVertex2i(currentPosition.x, currentPosition.y);
+  glEnd();
+
+  glFlush();
+}
+
+int main(int argc, char** argv) {
+  glutInit(&argc, argv); // initialize the Open-GL toolkit
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // set the display mode
+  glutInitWindowSize(windowWidth, windowHeight); // set window size
+  glutInitWindowPosition(43, 24); // centered for 1366 x 768 monitors
+  glutCreateWindow("Music Motion"); // open the screen window
+
+  // register the callback functions
+  glutDisplayFunc(myDisplay);
+  // glutReshapeFunc(myReshape);
+  // glutMouseFunc(myMouse);
+  glutPassiveMotionFunc(myMovedMouse);
+  // glutKeyboardFunc(myKeyboard);
+
+  myInit(); // additional initializations as necessary
+  glutMainLoop(); // go into a perpetual loop
+  return 0;
+}
